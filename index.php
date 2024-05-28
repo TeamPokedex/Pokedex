43db56ae6b759667b4c336cce026207482760e8e
Thunder Hime <takkikko@gmail.com>
2024-05-28 09:11:56 +0200

    php info en moins

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
 
-    php_info();
+switch ($_GET['q']) {
+    case 'value':
+        echo "ok";
+        break;
 
-    switch ($_GET['q']) {
-        case 'value':
-            echo "ok";
-            break;
-        
-        default:
-            echo "pas ok";
-            break;
-    }
-    
+    default:
+        echo "pas ok";
+        break;
+}
