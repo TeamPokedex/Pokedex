525ca3ed85bcf75018a863387fb7ffdcd3e90548
Thunder Hime <takkikko@gmail.com>
2024-05-29 16:28:36 +0200

    display type

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,44 @@
-let url = window.location.search;
-let param = url.split('=')[1];
-console.log(param); 
+// let url = window.location.search;
+// let param = url.split('=')[1];
+
+// function getQueryParam(param) {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     return urlParams.get(param);
+// }
+
+function getPokemonTypeColor(type) {
+    const typeColors = {
+        'Eau': 'blue',
+        'Feu': 'orange',
+        'Plante': 'green',
+        'Electrik': 'yellow',
+        'Psy': 'purple',
+        'Glace': 'lightblue',
+        'Dragon': 'red',
+        'Ténèbres': 'black',
+        'Fée': 'pink',
+        'Normal': 'gray',
+        'Combat': 'red',
+        'Vol': 'blue',
+        'Poison': 'purple',
+        'Sol': 'brown',
+        'Roche': 'gray',
+        'Insecte': 'green',
+        'Acier': 'gray',
+        'Spectre': 'black',
+    };
+    return typeColors[type] || 'white'; // couleur par défaut si le type n'est pas trouvé
+}
+
+function setBackgroundColor() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        const type = card.getAttribute('data-type');
+        const color = getPokemonTypeColor(type);
+        if (color) {
+            card.style.backgroundColor = color;
+        }
+    });
+}
+
+window.onload = setBackgroundColor;
--- views/pokemon/list.php
+++ views/pokemon/list.php
@@ -1,15 +1,22 @@
-<div class="container" id= list-container>
+<div class="container" id="list-container">
     <div class="row">
         <?php
-        // Affichage de tous les Pokémon de type Eau
         if (is_array($pokemonListDecode)) {
-            foreach ($pokemonListDecode as $value) { ?>
+            foreach ($pokemonListDecode as $value) {
+                // var_dump($value);
+                // die;
+        ?>
                 <div class="col-md-4 mb-4">
-                    <div class="card" style="width: 100%;">
+                    <div class="card" style="width: 100%;" data-type="<?= $value->type ?>">
                         <img src="<?= $value->image ?>" class="card-img-top" alt="<?= $value->name ?>">
                         <div class="card-body">
                             <h5 class="card-title"><?= $value->name ?></h5>
                             <p class="card-text">ID: <?= $value->id ?></p>
+                            <p class="card-text">Type: <?php 
+                            foreach ($value->apiTypes as $value) { ?>
+                              <p><?= $value->name?></p>
+                              <?php  
+                            }?></p>
                         </div>
                         <div class="card-footer">
                             <a href="./pokemonDetailsController.php?id=<?= $value->id ?>">Voir + de détails</a>
