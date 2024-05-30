f3edb208574f7349fe799496f1529ce156a3230c
xR3PMz <xr3pmz@gmail.com>
2024-05-30 13:51:04 +0200

    finished 404

--- partials/footer.php
+++ partials/footer.php
@@ -1,14 +1,14 @@
-<div class="footer">
-    <div class="row mt-3 open-sans-regular my-3">
-        <div class="col-12 col-xl-4 mt-2">
+<footer>
+    <div class="row open-sans-regular align-items-center align-content-center text-center">
+        <div class="col-12 col-md-4 mt-2">
             <div>Mentions légales</div>
         </div>
-        <div class="col-12 col-xl-4 mt-2">
+        <div class="col-12 col-md-4 mt-2">
             <div>Politique de confidentialités</div>
         </div>
-        <div class="col-12 col-xl-4 mt-2">
+        <div class="col-12 col-md-4 mt-2">
             <div>Conditions générales</div>
         </div>
     </div>
-    <div class="open-sans-light">Copyright - PokeWorld 2024</div>
-</div>
+    <div class="open-sans-light text-center col-12 mt-2">Copyright - PokeWorld 2024</div>
+</footer>
