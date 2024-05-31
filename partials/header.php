d72e70715ce4b6cf99c0ec4c8e7dc2549b98a37d
fredjacobbb <frdjacobbb@gmail.com>
2024-05-31 08:53:56 +0200

    8:53

--- partials/header.php
+++ partials/header.php
@@ -23,36 +23,42 @@
             </div>
             <div class="w-50 d-flex justify-content-end text-center">
                 <div class="mx-3">
-                    <i class="fa-solid fa-heart fs-4"></i>
-                    <p class="m-0 fs-12 text-light">Favoris</p>
+                    <i class="fa-solid fa-heart fs-4 w-100"></i>
+                    <a href="" class="m-0 fs-12 text-light">Favoris</a>
                 </div>
                 <div class="mx-3">
-                    <i class="fa-solid fa-user fs-4 mx-3"></i>
-                    <p class="m-0 fs-12 text-light">Profil</p>
+                    <i class="fa-solid fa-user fs-4 w-100"></i>
+                    <a href="" class="m-0 fs-12 text-light">Profil</a>
                 </div>
             </div>
             <div class="d-xl-none d-none mobile-nav-menu position-absolute start-0">
                 <ul class="my-2">
-                    <li class="mb-3 text-light">
-                        <a href="/">Accueil</a>
+                    <li class="mb-3">
+                        <a class="text-light my-5" href="/">Accueil</a>
                     </li>
-                    <li class="mb-3 text-light">
-                        <a href="">Pokédex</a>
+                    <li class="mb-3">
+                        <a class="text-light my-5" href="">Pokédex</a>
                     </li>
-                    <li class="mb-3 text-light">
-                        <a href="/?param">Paramètres</a>
+                    <li class="mb-3">
+                        <a class="text-light my-5" href="/?param">Paramètres</a>
+                    </li>
+                    <li class="mb-3">
+                        <div class="position-relative">
+                            <input type="text" placeholder="ex : pikachu" class="form-control input-search">
+                            <a class="fa-solid text-secondary text-decoration-none search-bar-menu position-absolute fa-search me-2 ps-2"></a>
+                        </div>
                     </li>
                 </ul>
             </div>
         </nav>
-        <nav class="d-none d-xl-flex justify-content-between align-items-center">
+        <nav class="d-none d-md-flex justify-content-between align-items-center">
             <div class="menu-image py-1">
                 <img src="/public/assets/img/logoPokeworld.png" alt="">
             </div>
 
             <p class="position-relative w-25">
                 <input type="text" placeholder="ex : pikachu" class="form-control input-search">
-                <a class="fa-solid text-secondary text-decoration-none search-bar-menu position-absolute fa-search end-0 me-2 ps-2"></a>
+                    <a class="fa-solid text-secondary text-decoration-none search-bar-menu position-absolute fa-search fa-search-md me-2"></a>
                 </input>
             </p>
 
@@ -60,12 +66,12 @@
             <a href="" class="text-light fw-bold">POKEDEX</a>
             <div class="d-flex flex-wrap justify-content-end">
                 <div class="mx-3 text-center">
-                    <i class="fa-solid fa-heart fs-2"></i>
-                    <p class="m-0 fs-12 text-light">Favoris</p>
+                    <i class="fa-solid fa-heart fs-2 w-100"></i>
+                    <a href="" class="m-0 fs-12 text-light">Favoris</p>
                 </div>
                 <div class="mx-3 text-center">
-                    <i class="fa-solid fa-heart fs-2"></i>
-                    <p class="m-0 fs-12 text-light">Profil</p>
+                    <i class="fa-solid fa-heart fs-2 w-100"></i>
+                    <a href="" class="m-0 fs-12 text-light">Profil</p>
                 </div>
             </div>
         </nav>
